553872578a8396629b1e31a8cbefbc02a4e4c841
Eliezer Nascimento <eliezergarbin1@gmail.com>
2022-12-13 22:58:43 -0300

    estilizando header e tabBar

--- mobile/src/routes/stackRoutes.tsx
+++ mobile/src/routes/stackRoutes.tsx
@@ -1,4 +1,7 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
 import { VotesScreen } from "../screens/VotesScreen";
 import { WinnerScreen } from "../screens/WinnerScreen";
 
@@ -6,10 +9,46 @@ const { Screen, Navigator } = createBottomTabNavigator();
 
 export function StackRoutes() {
   return (
-    <Navigator>
-      <Screen name="Votes" component={VotesScreen} />
+    <Navigator
+      screenOptions={({ route }) => ({
+        headerShown: true,
+        headerStyle: {
+            backgroundColor: "rgba(34,36,40,1)",
+        },
+        headerTintColor: '#fff',
+        tabBarStyle: {
+          height: 70,
+          paddingHorizontal: 2,
+          paddingTop: 0,
+          backgroundColor: "rgba(34,36,40,1)",
+          position: "absolute",
+          borderTopWidth: 0,
+        },
+      })}
+    >
+      <Screen
+        name="Votes"
+        component={VotesScreen}
+        options={{
+          tabBarLabel: "Vote",
+          tabBarActiveTintColor: "white",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color="#6DFF60" size={size} />
+          ),
+        }}
+      />
 
-      <Screen name="Winner" component={WinnerScreen} />
+      <Screen
+        name="Winner"
+        component={WinnerScreen}
+        options={{
+          tabBarLabel: "Winner",
+          tabBarActiveTintColor: "white",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="trophy" color="#6DFF60" size={size} />
+          ),
+        }}
+      />
     </Navigator>
   );
 }
